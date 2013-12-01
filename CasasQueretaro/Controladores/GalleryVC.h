82b7cdb110f96082428b9a2bac5f004b407eac36
Gerardo Reyes <grevych@MacBook-Pro-de-Gerardo.local>
2013-12-01 15:44:34 -0600

    Despliegue de galeria

---
+++ CasasQueretaro/Controladores/GalleryVC.h
@@ -0,0 +1,22 @@
+//
+//  GalleryVC.h
+//  CasasQueretaro
+//
+//  Created by Gerardo Reyes on 18/11/13.
+//  Copyright (c) 2013 Guillermo Hernandez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "UIViewAnimationExplose.h"
+
+
+@interface GalleryVC : UIViewController
+
+
+
+- (id)init;
+
+
+@property (nonatomic, retain) NSMutableArray *views;
+
+@end
